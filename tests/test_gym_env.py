68368f19a590cec03c6f42d4112e07499b2e674c
Justin Smith <jvstinian@gmail.com>
2024-12-17 09:31:04 -0500

    Fixing the test.

--- tests/test_gym_env.py
+++ tests/test_gym_env.py
@@ -4,17 +4,15 @@ from zombpyg.gym_env import ZombpygGymEnv
 
 
 def test_gym_env_observation():
-    gym_env = ZombsoleGymEnv(
+    gym_env = ZombpygGymEnv(
         map_id="demo",
         rules_id="survival",
         player_specs="terminator:knife:0",
         initial_zombies=0,
         minimum_zombies=0, 
         verbose=False
-    ):
     )
-    observation = gym_env.get_observation()
-    sensor_count = len(gym_env.game.agent_builder.sensor_specs)
-    sensor_channels = 7
-    assert observation.shape == (1,sensor_count*sensor_channels,2)
+    observation = gym_env.reset()
+    feedback_size = gym_env.game.agent_builder.get_feedback_size()
+    assert observation.shape == (1,feedback_size,1)
 
