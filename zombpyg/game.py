fc14f77c1b5b4df8cdec0b28ae7f5ff9bec8ea66
jvstinian <jvstinian@gmail.com>
2024-12-23 10:04:00 -0500

    gym environment continued (#10)
    Continuing with improvements to the gymnasium environment. Improving the rendering support.

--- zombpyg/game.py
+++ zombpyg/game.py
@@ -193,7 +193,6 @@ class Game:
 
         self.spawn_zombies_to_maintain_minimum()
        
-        # TODO: Switch to returning the following
         done = False
         truncated = False
 
@@ -216,7 +215,7 @@ class Game:
                 print("GAME OVER.  Reached 300 seconds, stopping.")
             truncated = True
 
-        return reward, feedbacks.reshape((1, len(feedbacks), 1)), (truncated or done)
+        return reward, feedbacks.reshape((1, len(feedbacks), 1)), done, truncated
     
     # This is used by the train method
     def get_total_reward(self):
