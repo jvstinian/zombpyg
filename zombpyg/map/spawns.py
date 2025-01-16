13b460830a6035b5e15188e954eaddd6e9f302e1
Justin Smith <jvstinian@gmail.com>
2025-01-16 09:17:28 -0500

    Adding spawn locations used only during game initialization.

--- zombpyg/map/spawns.py
+++ zombpyg/map/spawns.py
@@ -3,11 +3,12 @@ from zombpyg.core.things import RectangularThing
 
 
 class SpawnLocation(RectangularThing):
-    def __init__(self, left, top, width, height):
+    def __init__(self, left, top, width, height, initial_spawn_only=False):
         super(SpawnLocation, self).__init__(
             left, top, width, height,
             'objective', 'green', 0, 
         )
+        self.initial_spawn_only = initial_spawn_only
     
     def get_spawn_location(self):
         x = numpy.random.randint(self.left, self.left + self.width)
