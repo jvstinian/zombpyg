b3f0583915837f7d76527c1a5c6f490a8ac29e3b
Justin Smith <jvstinian@gmail.com>
2025-01-17 17:05:46 -0500

    Starting to adapt the multiagent environment to the PettingZoo parallel environment interface.

--- zombpyg/multiagent_env.py
+++ zombpyg/multiagent_env.py
@@ -12,6 +12,7 @@ import time
 import numpy as np
 
 
+# TODO: When we update from nixos-23.05, we will need to make sure this properly conforms with PettingZoo's ParallelEnv.
 class MultiagentZombsoleEnv(object):
     """The main Gym class for multiagent play.
     """
@@ -21,13 +22,13 @@ class MultiagentZombsoleEnv(object):
     }
     
     # Set these in ALL subclasses
-    reward_range = (-float('inf'), float('inf'))
-    action_space = Sequence(
-        Dict({
-            "agent_id": Discrete(64),
-            "action": Discrete(AgentActions.get_actions_n())
-        })
-    )
+    reward_range = (-float('inf'), float('inf')) # NOTE: For gym, is this needed for pettingzoo?
+    # action_space = Sequence(
+    #     Dict({
+    #         "agent_id": Discrete(64),
+    #         "action": Discrete(AgentActions.get_actions_n())
+    #     })
+    # )
 
     def __init__(self, 
         map_id="demo",
@@ -56,60 +57,69 @@ class MultiagentZombsoleEnv(object):
         self.window = None
         self.__initialize_renderer__()
 
-        self.observation_space = self._get_observation_space()
+        self.agents = agent_ids
+        self.possible_agents = agent_ids
+        # self.num_agents = len(self.agents) # Is this needed by PettingZoo?
+        # self.max_num_agents = len(self.possible_agents) # Is this needed by PettingZoo?
+        self.action_spaces = { agend_id: Discrete(AgentActions.get_actions_n()) for agent_id in self.possible_agents }
+        self.observation_spaces = self._get_observation_spaces()
+
+    # Using default implementations of 
+    # observation_space(agent: AgentID) -> Space
+    # and 
+    # action_space(agent: AgentID) -> Space
+    # NOTE: PettingZoo also specifies a method `state() -> ndarray` for returning a global view.
+    #       This will be left unimplemented for now.
 
     # setting observation_space in the constructor with the help of the following
-    def _get_observation_space(self):
-        return Sequence(
-            Dict({
-                "agent_id": Discrete(64),
-                "observation": Box(low=0.0, high=400.0, shape=self.game.get_feedback_size())
-            })
-        )
+    def _get_observation_spaces(self):
+        return { agend_id: Box(low=0.0, high=400.0, shape=self.game.get_feedback_size()) for agent_id in self.possible_agents }
+        # return Sequence(
+        #     Dict({
+        #         "agent_id": Discrete(64),
+        #         "observation": Box(low=0.0, high=400.0, shape=self.game.get_feedback_size())
+        #     })
+        # )
 
     def get_observation(self):
-        ret = []
+        ret = {}
         for agent in self.game.world.agents:
-            agentobs = agent.sensor_feedback()
-            agentobs = agentobs.reshape((1, len(agentobs), 1))
-            ret.append(
-                {
-                    "agent_id": agent.agent_id,
-                    "observation": agent.sensor_feedback().reshape((1, len(feedbacks), 1))
-                } 
-            )
+            if agent.agent_id in self.agents: # This indicates the agent was alive before the step
+                agentobs = agent.sensor_feedback()
+                agentobs = agentobs.reshape((1, len(agentobs), 1))
+                ret[agent.agent_id] = agent.sensor_feedback().reshape((1, len(feedbacks), 1))
         return ret
 
     # Taken from zombpyg single-agent gym.  Is this needed here?
     # def get_frame_size(self):
     #     return self.game.get_feedback_size()
 
-    def _process_action(self, action):
-        return {
-            v["agent_id"]: v["action"] for v in action
-        }
+    # def _process_action(self, action):
+    #     return {
+    #         v["agent_id"]: v["action"] for v in action
+    #     }
 
     def step(self, action):
-        """Run one timestep of the environment's dynamics. When end of
-        episode is reached, you are responsible for calling `reset()`
-        to reset this environment's state.
+        """
+        Receives a dictionary of actions keyed by the agent name.
 
-        Accepts an action and returns a tuple (observation, reward, done, info).
+        Returns the observation dictionary, reward dictionary, terminated dictionary, truncated dictionary and info dictionary,
+        where each dictionary is keyed by the agent.
 
         Args:
-            action (object): an action provided by the agent
+            actions (dict): A dictionary of actions with agent IDs as keys
 
         Returns:
-            observation (object): agent's observation of the current environment
-            reward (float) : amount of reward returned after previous action
+            observation (dict): observations of the current environment for each agent ID
+            reward (dict[AgentID, float]) : amount of reward returned after previous action for each agent ID
             done (bool): whether the episode has ended, in which case further step() calls will return undefined results
-            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning)
+            truncated (bool): whether the episode has expired without a clear outcome, in which case further step() calls will return undefined results
+            info (dict of dicts): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning) for each agent ID
         """
-        agent_actions_map = self._process_action(action)
         agent_actions = []
         for agent in self.game.world.agents:
             agent_actions.append(
-                agent_actions_map.get(
+                actions.get(
                     agent.agent_id,
                     AgentActions,get_no_action_id()
                 )
@@ -117,6 +127,13 @@ class MultiagentZombsoleEnv(object):
         # TODO: The following Game method supports only a single agent action
         reward, observation, done, truncated = self.game.play_action(agent_actions)
         info = {}
+
+        # form returns
+        # for self.players specify done and truncated
+        # empyt imfo for all players
+        # make sure observations are properly formatted
+
+        # filter self.agents to only those that are still living
 
         return observation, reward, done, truncated, info
 
@@ -158,38 +175,39 @@ class MultiagentZombsoleEnv(object):
             pygame.display.quit()
             pygame.quit()
 
-    def seed(self, seed=None):
-        """Sets the seed for this env's random number generator(s).
+    # def seed(self, seed=None):
+    #     """Sets the seed for this env's random number generator(s).
 
-        Note:
-            Some environments use multiple pseudorandom number generators.
-            We want to capture all such seeds used in order to ensure that
-            there aren't accidental correlations between multiple generators.
+    #     Note:
+    #         Some environments use multiple pseudorandom number generators.
+    #         We want to capture all such seeds used in order to ensure that
+    #         there aren't accidental correlations between multiple generators.
 
-        Returns:
-            list<bigint>: Returns the list of seeds used in this env's random
-              number generators. The first value in the list should be the
-              "main" seed, or the value which a reproducer should pass to
-              'seed'. Often, the main seed equals the provided 'seed', but
-              this won't be true if seed=None, for example.
-        """
-        # NOTE: Not currently capturing the seed information used in zombsole
-        return
+    #     Returns:
+    #         list<bigint>: Returns the list of seeds used in this env's random
+    #           number generators. The first value in the list should be the
+    #           "main" seed, or the value which a reproducer should pass to
+    #           'seed'. Often, the main seed equals the provided 'seed', but
+    #           this won't be true if seed=None, for example.
+    #     """
+    #     # NOTE: Not currently capturing the seed information used in zombsole
+    #     return
 
-    @property
-    def unwrapped(self):
-        """Completely unwrap this env.
+    # @property
+    # def unwrapped(self):
+    #     """Completely unwrap this env.
 
-        Returns:
-            gym.Env: The base non-wrapped gym.Env instance
-        """
-        return self
+    #     Returns:
+    #         gym.Env: The base non-wrapped gym.Env instance
+    #     """
+    #     return self
 
     def __str__(self):
-        if self.spec is None:
-            return '<{} instance>'.format(type(self).__name__)
-        else:
-            return '<{}<{}>>'.format(type(self).__name__, self.spec.id)
+        # if self.spec is None:
+        #     return '<{} instance>'.format(type(self).__name__)
+        # else:
+        #     return '<{}<{}>>'.format(type(self).__name__, self.spec.id)
+        return '<{} instance>'.format(type(self).__name__)
 
     def __enter__(self):
         """Support with-statement for the environment. """
