3aaf44110149a12415c43ea06dbf35fe84927d51
Justin Smith <jvstinian@gmail.com>
2025-02-19 08:49:30 -0500

    Cleaning up the world configuration builder code.

--- zombpyg/map/world_config_builder.py
+++ zombpyg/map/world_config_builder.py
@@ -9,15 +9,13 @@ from zombpyg.map.map import MapFactory
 class GameState(StrEnum):
     UNINITIALIZED = "uninitialized"
     INITIALIZED = "initialized"
-    TRUNCATED = "game_truncated" # TODO: Is this needed?
+    TRUNCATED = "game_truncated"
     GAME_LOST = "game_lost"
     GAME_WON = "game_won"
 
 class WorldConfiguration(object):
     def __init__(self, game_map: str, initial_zombies: int, minimum_zombies: int):
         self.game_map = game_map
-        # self.w = w
-        # self.h = h
         self.initial_zombies = initial_zombies 
         self.minimum_zombies = minimum_zombies
 
@@ -86,12 +84,6 @@ class SingleMapBuilder(WorldConfigurationBuilder):
     def get_render_height(self):
         return self.h
     
-    # def get_initial_zombies(self):
-    #     return self.initial_zombies
-
-    # def get_minimum_zombies(self):
-    #     return self.minimum_zombies
-    
 class RandomMapBuilder(WorldConfigurationBuilder):
     def __init__(self, weights: List[float], map_builders: List[WorldConfigurationBuilder]):
         self.weights = weights
@@ -122,28 +114,18 @@ class RandomMapBuilder(WorldConfigurationBuilder):
         return max([mb.get_render_height() for mb in self.map_builders])
 
 class WorldConfigurationBuilderFactory(object):
-    # @staticmethod
-    # def get_map_builder_from_json(jsonobj: str):
-    #     try:
-    #         obj = json.loads(jsonobj, object_hook=MapBuilder.decode_hook)
-    #         if not isinstance(obj, (MapBuilder,)):
-    #             raise ValueError(f"Expected parsed JSON to be of type MapBuilder, but has type {type(obj)} instead")
-    #     except Exception as ex:
-    #         raise ex
-    #     else:
-    #         return obj
-
     @staticmethod
     def get_world_configuration_builder(map_configuration: Dict):
         if "tag" in map_configuration:
             if (map_configuration["tag"] in ["SingleMap", "RandomMap"]) and ("parameters" not in map_configuration):
                 raise ValueError(f"A WorldConfigurationBuilder with tag {map_configuration['tag']} must have key \"parameters\"")
+
             if map_configuration["tag"] == "SingleMap":
                 return SingleMapBuilder.from_dict(map_configuration["parameters"])
-            if map_configuration["tag"] == "RandomMap":
+            elif map_configuration["tag"] == "RandomMap":
                 mb = RandomMapBuilder.from_list_of_dicts(map_configuration["parameters"])
                 if any([weight <= 0 for weight in mb.weights]):
-                    raise ValueError("A negative weight was encountered in a RandomMapBuilder")
+                    raise ValueError("A non-positive weight was encountered in a RandomMapBuilder")
                 return mb
             else:
                 raise ValueError(f"GameRequest \"tag\" must be in {WorldConfigurationBuilder.valid_tags()}")
